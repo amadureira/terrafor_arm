9516fee86f4bff5e5f82a6c6c0b51e4ab9dca3d8
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-13 17:08:34 -0300

    Add dependencias

--- projects/project_1/network.tf
+++ projects/project_1/network.tf
@@ -2,12 +2,19 @@
 data "azurerm_virtual_network" "networkname" {
    name = "teste-network"
    resource_group_name = "${azurerm_resource_group.networkresource.name}"
+   depends_on = [ 
+      azurerm_resource_group.appresource
+   ] 
+
 }
 
 data "azurerm_subnet" "subnetname" {
   name                 =  "testsubnet"
   virtual_network_name =  data.azurerm_virtual_network.networkname.name
   resource_group_name = "${azurerm_resource_group.networkresource.name}"
+   depends_on = [ 
+    data.azurerm_virtual_network.networkname
+   ]
 }
 
 
@@ -21,7 +28,9 @@ resource "azurerm_network_interface" "interface1" {
     subnet_id                     = data.azurerm_subnet.subnetname.id
     private_ip_address_allocation = "Dynamic"
   }
-
+  depends_on = [ 
+   data.azurerm_subnet.subnetname
+  ]
   tags = {
     environment = "staging"
   }
--- projects/project_1/resourcegroup.tf
+++ projects/project_1/resourcegroup.tf
@@ -2,7 +2,9 @@ resource "azurerm_resource_group" "appresource" {
   name     = "test_resource_group"
   location = "West US"
 }
+
 resource "azurerm_resource_group" "networkresource" {
   name     = "production"
   location = "West US"
 }
+
