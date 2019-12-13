392ec7cf2719d9e8180a5f4f7bb27d0b282995fe
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-13 12:23:55 -0300

    fix group name

--- projects/modules/network/network.tf
+++ projects/modules/network/network.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "example" {
 
 resource "azurerm_subnet" "example" {
   name = "testsubnet"
-  resource_group_name  = "${azurerm_resource_group.example.name}" 
+  resource_group_name  = "${azurerm_resource_group.network.name}" 
   virtual_network_name = "teste-network"
   address_prefix = "10.0.1.0/24"
 }
