deaa2d4f24b82ad6cb8fac2de77b563e1b361eae
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-13 14:32:16 -0300

    Comment

--- projects/project_1/network.tf
+++ projects/project_1/network.tf
@@ -14,27 +14,27 @@ output "teste" {
   value = data.azurerm_virtual_network.networkname
 }
 /*
-data "azurerm_subnet" "subnetname" {
-  filter { 
-     name = "testsubnet"
-  }
-  virtual_network_id = data.azurerm_virtual_network.networkname.id
-}
-
-resource "azurerm_network_interface" "interface_1" {
-  name                = "interface_1"
-  location            = "${azurerm_resource_group.appresource.location}"
-  resource_group_name = "${azurerm_resource_group.appresource.name}"
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.subnetname.id}"
-    private_ip_address_allocation = "Dynamic"
-  }
-
-  tags = {
-    environment = "staging"
-  }
-}
+#data "azurerm_subnet" "subnetname" {
+#  filter { 
+#     name = "testsubnet"
+#  }
+#  virtual_network_id = data.azurerm_virtual_network.networkname.id
+#}
+#
+#resource "azurerm_network_interface" "interface_1" {
+#  name                = "interface_1"
+#  location            = "${azurerm_resource_group.appresource.location}"
+#  resource_group_name = "${azurerm_resource_group.appresource.name}"
+#
+#  ip_configuration {
+#    name                          = "testconfiguration1"
+#    subnet_id                     = "${azurerm_subnet.subnetname.id}"
+#    private_ip_address_allocation = "Dynamic"
+#  }
+#
+#  tags = {
+#    environment = "staging"
+#  }
+#}
 
 */
