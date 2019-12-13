232b176e7a65f3708a8ee3435671fd9b7cddeae2
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-13 15:04:13 -0300

    Change network config

--- projects/project_1/network.tf
+++ projects/project_1/network.tf
@@ -6,35 +6,27 @@ resource "azurerm_resource_group" "appresource" {
 data "azurerm_virtual_network" "networkname" {
    name = "teste-network"
    resource_group_name = "production"
-   
-
 }
+
 data "azurerm_subnet" "subnetname" {
-  filter { 
-     name = "testsubnet"
+  name                 =  "testsubnet"
+  virtual_network_name =  data.azurerm_virtual_network.networkname
+  virtual_network_id   =  data.azurerm_virtual_network.networkname.id
+}
+
+
+resource "azurerm_network_interface" "interface_1" {
+  name                = "interface_1"
+  location            = "${azurerm_resource_group.appresource.location}"
+  resource_group_name = "${azurerm_resource_group.appresource.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.subnetname.id}"
+    private_ip_address_allocation = "Dynamic"
   }
-  virtual_network_id = data.azurerm_virtual_network.networkname.id
+
+  tags = {
+    environment = "staging"
+  }
 }
-
-output "teste" {
-  value = data.azurerm_virtual_network.networkname
-}
-
-/*
-#resource "azurerm_network_interface" "interface_1" {
-#  name                = "interface_1"
-#  location            = "${azurerm_resource_group.appresource.location}"
-#  resource_group_name = "${azurerm_resource_group.appresource.name}"
-#
-#  ip_configuration {
-#    name                          = "testconfiguration1"
-#    subnet_id                     = "${azurerm_subnet.subnetname.id}"
-#    private_ip_address_allocation = "Dynamic"
-#  }
-#
-#  tags = {
-#    environment = "staging"
-#  }
-#}
-
-*/
