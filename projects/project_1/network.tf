3a8f09b4cc6cdf63947215b69ba181fc01db8860
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-13 14:03:19 -0300

    Add new intes

--- projects/project_1/network.tf
+++ projects/project_1/network.tf
@@ -1,3 +1,7 @@
+resource "azurerm_resource_group" "appresource" {
+  name     = "test_resource_group"
+  location = "West US"
+}
 
 data "azurerm_virtual_network" "networkname" {
        tags  {
@@ -12,3 +16,21 @@ data "azurerm_subnet" "subnetname" {
   }
   virtual_network_name = data.azurerm_virtual_network.networkname.name
 }
+
+resource "azurerm_network_interface" "interface_1" {
+  name                = "interface_1"
+  location            = "${azurerm_resource_group.appresource.location}"
+  resource_group_name = "${azurerm_resource_group.appresource.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.subnetname.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+
