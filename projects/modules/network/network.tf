c9c79284048e5c8ec5db9b1eb86b9a595b7c3ced
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-13 12:21:50 -0300

    Add resource group

--- projects/modules/network/network.tf
+++ projects/modules/network/network.tf
@@ -1,9 +1,13 @@
+resource "azurerm_resource_group" "network" {
+  name     = "production"
+  location = "West US"
+}
 
 
 resource "azurerm_virtual_network" "example" {
   name                = "teste-network"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.network.name}"
+  location            = "${azurerm_resource_group.network.location}"
   address_space       = ["10.0.0.0/16"]
 }
 
