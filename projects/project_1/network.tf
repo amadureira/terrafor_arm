a7fd21f39cacb90723bb99512e009fedf2a9dbfe
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-16 16:47:23 -0300

    change container name

--- projects/project_1/network.tf
+++ projects/project_1/network.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "RG_prd_CC_abc_abc"
     storage_account_name = "asdfasdfsss"
-    container_name       = "abc123"
+    container_name       = "abc123sss"
     key                  = "key123abc"
   }
 }
