be1714f91ae0833f54bbfc81fae3b828b956813f
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-12-19 16:09:45 -0300

    Uncomment

--- projects/modules/vmware/network.tf
+++ projects/modules/vmware/network.tf
@@ -15,7 +15,7 @@ data "vsphere_compute_cluster" "compute_cluster" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-#resource "vsphere_resource_pool" "resource_pool" {
-#  name                    = "terraform-resource-pool-test"
-#  parent_resource_pool_id = "${data.vsphere_compute_cluster.compute_cluster.resource_pool_id}"
-#}
+resource "vsphere_resource_pool" "resource_pool" {
+  name                    = "terraform-resource-pool-test"
+  parent_resource_pool_id = "${data.vsphere_compute_cluster.compute_cluster.resource_pool_id}"
+}
