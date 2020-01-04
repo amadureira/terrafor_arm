0741d73e1980474313b807cdd092a7fad3faa2d6
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2020-01-04 16:18:35 -0300

    Remove var

--- projects/modules/network/backend.tf
+++ projects/modules/network/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "RG_prd_CC_abc_abc"
     storage_account_name = "asdfasdfsss"
-    container_name       = "${var.workspace_name}"
+    container_name       = "project"
     key                  = "prod.terraform.tfstate"
   }
 }
