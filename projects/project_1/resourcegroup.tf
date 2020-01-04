f63cd168e1feaec165a95a31ed696ab1814b1f8a
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2020-01-04 17:56:32 -0300

    Change resource group declare

--- projects/project_1/resourcegroup.tf
+++ projects/project_1/resourcegroup.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "appresource" {
   location = "West US"
 }
 
-resource "azurerm_resource_group" "networkresource" {
+data "azurerm_resource_group" "networkresource" {
   name     = "production"
-  location = "West US"
+# location = "West US"
 }
 
