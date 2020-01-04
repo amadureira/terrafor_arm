cc6a49547fbcf2906c93924e9fb3ab9591d53149
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2020-01-04 17:56:14 -0300

    change resource group references

--- projects/project_1/network.tf
+++ projects/project_1/network.tf
@@ -2,7 +2,7 @@
 data "azurerm_subnet" "subnetname" {
   name                 = "testsubnet"
   virtual_network_name = "teste-network"
-  resource_group_name  = "${azurerm_resource_group.networkresource.name}"
+  resource_group_name  = "${data.azurerm_resource_group.networkresource.name}"
 
 }
 
